f1b81db15a69a8387b49b2b99475aa4ce166dc9e
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-11 19:05:41 +0000

    Implemented validations to User and changed sign-up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :gender, presence: true, blank: false,  inclusion: { in: GENDERS }
+  validates :gender, presence: true, blank: false, inclusion: { in: GENDERS }
+  validates :age, presence: true, :numericality => { :only_integer => true, greater_than: 18 }
   mount_uploader :photo, PhotoUploader
 
+  # Meal
   has_many :meal_availabilities
   has_many :meal_events, through: :meal_availabilities
+  # Interests
+  has_many :user_interests
+  has_many :interests, through: :user_interests
+  # Dietary Preferences
+  has_may :user_cuisines
+  has_may :cuisines, through: :user_cuisines
+
+  # Contacts
+  has_many :user_contacts
+
+  # Location
+  has_one :city
+  has_one :country, through: :city
 end
